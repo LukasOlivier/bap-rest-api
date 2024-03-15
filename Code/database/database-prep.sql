8454bc8c34ba6cd697ff726e2a66c48e31ea25e2
Lukas O. <olivier.lukas2003@gmail.com>
2024-03-15 08:39:00 +0100

    improved openapi-spec

--- Code/database/database-prep.sql
+++ Code/database/database-prep.sql
@@ -1,7 +1,6 @@
-DROP TABLE IF EXISTS Users;
-DROP TABLE IF EXISTS Courses;
-DROP TABLE IF EXISTS Schools;
 DROP TABLE IF EXISTS Students;
+DROP TABLE IF EXISTS Schools;
+DROP TABLE IF EXISTS Users;
 
 CREATE TABLE Users (
     id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
@@ -16,23 +15,14 @@ CREATE TABLE Schools (
     FOREIGN KEY  (ownerId) REFERENCES Users(id)
 )
 
-CREATE TABLE Courses (
-    id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
-    name VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    credits INT,
-    schoolId INT NOT NULL,
-    FOREIGN KEY  (schoolId) REFERENCES Schools(id)
-)
-
 CREATE TABLE Students (
   id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
   name VARCHAR(255) NOT NULL,
   age INT,
   email VARCHAR(255) UNIQUE,
   phone VARCHAR(255),
-  courseId INT,
-  FOREIGN KEY  (courseId) REFERENCES Courses(id)
+  schoolId INT,
+  FOREIGN KEY (schoolId) REFERENCES Schools(id)
 );
 
 INSERT INTO Users (email, password)
@@ -40,32 +30,11 @@ VALUES ('user1@example.com', 'password'),
        ('user2@example.com', 'password');
 
 INSERT INTO Schools (name, ownerId)
-VALUES ('Harvard University', 2),
-       ('MIT', 1);
+VALUES ('Harvard University', 1),
+       ('MIT', 2);
 
-INSERT INTO Courses (name, description, credits, schoolId)
-VALUES ('Math 101', 'Introduction to Algebra', 3, 1),
-       ('English 101', 'Introduction to English', 3, 1),
-       ('History 101', 'Introduction to World History', 3, 1),
-       ('Sql 101', 'Introduction to SQL', 3, 2),
-       ('Python 101', 'Introduction to Python', 3, 2),
-       ('Java 101', 'Introduction to Java', 3, 2);
-
-
-INSERT INTO Students (name, age, email, phone, courseId)
+INSERT INTO Students (name, age, email, phone, schoolId)
 VALUES ('Alice Smith', 20, 'alice.smith@example.com', '+1234567890', 1),
-       ('Bob Jones', 22, 'bob.jones@example.com', '+1987654321', 2),
-       ('Charlie Brown', 21, 'charlie.brown@example.com', '+1123456789', 4),
-       ('Daisy Johnson', 23, 'daisy.johnson@example.com', '+1157856799', 6);
-
-SELECT * FROM Schools 
-JOIN Users ON Schools.ownerId = Users.id
-WHERE ownerId = 2
-
--- Students per school
-SELECT *
-FROM Schools
-JOIN Courses ON Schools.id = Courses.schoolId
-JOIN Students ON Courses.id = Students.courseId
-
-SELECT * FROM Students WHERE courseId IN (SELECT id FROM Courses WHERE schoolId = 2)
+       ('Bob Jones', 22, 'bob.jones@example.com', '+1987654321', 1),
+       ('Charlie Brown', 21, 'charlie.brown@example.com', '+1123456789', 2),
+       ('Daisy Johnson', 23, 'daisy.johnson@example.com', '+1157856799', 2);
